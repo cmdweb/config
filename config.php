bd7f2c9af19e2f359f638f4d25c78e797852cc4a
Gabriel Malaquias <gemalaquias@gmail.com>
2015-03-25 14:20:39 -0300

    Ajustes Config

--- config.php
+++ config.php
@@ -1,101 +1,43 @@
 <?php
-/**
- * Arquivo de configuração Alcatraz Framework
- *
- * Dependendo dos paths importandos para seu projeto não é necessários configurar todas
- * as constantes.
- *
- * NAO REMOVER NENHUM CAMPO DESTE ARQUIVO, SE NECESSARIO APENAS FAZER ALTERAÇÕES.
- */
-
-
-/**
- * DEFAULT timezone
- */
 date_default_timezone_set('America/Sao_Paulo');
-
+/**
+ * NAO REMOVER NENHUM CAMPO DESTE ARQUIVO, SE NECESSARIO APENAS FAZER ALTERAÇÕES
+ */
 /**
  * DEFAULT para PATH
  */
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT',dirname(dirname(dirname(__DIR__))) . DS);
-
+define('ROOT', dirname(dirname(dirname(__DIR__))). DS);
 /**
  * DEFINIÇÃO DE CONSTANTES PARA URL
  */
 define('URL_PUBLIC_FOLDER', 'public');
-
 define('URL_PROTOCOL', 'http://');
 define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
 define('URL_SUB_FOLDER', str_replace(URL_PUBLIC_FOLDER, '', dirname($_SERVER['SCRIPT_NAME'])));
 define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
-
-
 /**
- * Pasta onde fica os controllers, views, areas e entities
- * caso seja alterada aqui, deve ser alterada no composer.json e
- * executar o update do composer para que os namespaces das classes
- * sejam encontradas.
+ * Caso mudar o padrao Application, mudar no composer e gerar os autoloads
  */
 define('FOLDER_SRC','Application');
-
-/**
- * Caminho fisico até a pasta onde se encontram os controllers,
- * areas, entities e views.
- */
 define('APP', ROOT . FOLDER_SRC . DS);
-
-/**
- * Caminho fisico até a pasta vendor.
- * Caso tenha optado por outro nome deve ser alterado aqui.
- */
 define('VENDOR', ROOT . 'vendor' . DS);
 
-/**
- * Caminho fisico até a pasta public, pasta que contem os arquivos
- * que podem ser visualizados no acesso ao seu site.
- */
-define('PATH_PUBLIC', ROOT . 'public' . DS);
-
-/**
- * Caminho fisico até a pasta layouts que contem as maters pages
- * que podem ser selecionadas.
- */
-define('PATH_LAYOUT', APP . 'layouts' . DS);
-
-/**
- * Caminho fisico até a pasta alcatraz na vendor
- */
-define('ALCATRAZ_PATH', VENDOR . "alcatraz" . DS);
-
-/**
- * Caminho fisico até os Validators que estão contidos no PATH ModelState.
- */
-define('VALIDATORS_PATH', ALCATRAZ_PATH . "modelstate" . DS . "ModelState" . DS . "Validators" . DS);
-
-/**
- * Caminho fisico até os Controllers, Views, Models, Areas e Entities
- */
-define('PATH_CONTROLLER', APP . 'Controllers' . DS);
-define('PATH_VIEWS',      APP . 'Views' . DS);
-define('PATH_MODELS',     APP . 'Models' . DS);
 define('PATH_AREA',       APP . 'Areas' . DS);
-define('PATH_ENTITIES',       APP . 'Entities' . DS);
-
+define('PATH_CONTROLLER', APP . 'Controllers' . DS);
+//define('PATH_LAYOUT', PATH_PUBLIC . 'layouts' . DS);
+define('PATH_VIEWS',      APP . 'Views' . DS);
+define("LOG_PATH" , VENDOR .DS. "alcatraz" . DS . "logs");
 /**
- * Namespaces dos Controllers, Views, Models, Areas e Entities
+ * Namespaces
  */
-define('NAMESPACE_CONTROLLER', 'Controllers');
-define('NAMESPACE_MODELS', 'Models');
 define('NAMESPACE_AREAS', 'Areas');
-define('NAMESPACE_VALIDATORS', 'ModelState\\Validators\\');
-define('NAMESPACE_ENTITIES', 'Entities\\');
+define('NAMESPACE_CONTROLLER', 'Controllers');
 
 /**
  * VARIAVEL DE TIMESTAMP PARA PADRÃO DE ATRIBUTOS
  */
 define("CURRENT_TIMESTAMP", date('Y-m-d H:i:s'));
-
 /**
  * DEFINIÇÂO DAS VARIAVEIS DE CONEXÂO COM O BD
  */
@@ -104,14 +46,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'alcatraz');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-
-/**
- * Define os dados para conexão com o servidor de email para o envio de email atravez da Classe alcatraz/emailsend
- */
-define("HOST_EMAIL","");
-define("USER_EMAIL","");
-define("PASS_EMAIL","");
-
+define('DB_CHARSET', 'utf8');
 
 /**
  * PREFIX PARA ADICIONAR NA ACTION QUANDO EXISTE POST NA TELA
@@ -119,15 +54,7 @@ define("PASS_EMAIL","");
 define("PREFIX_POST", "_post");
 
 /**
- * DEFAULT Constroller,View
- */
-define("DEFAULT_CONTROLLER","IndexController");
-define("DEFAULT_CONTROLLER_ABV","Index");
-define("DEFAULT_VIEW","Index");
-
-
-/**
- * Constantes para a classe Layout.
+ * LAYOUT
  */
 define('PREFIX_TITLE', 'MVC');
 define("DEFAULT_LAYOUT", "basic");
@@ -137,36 +64,29 @@ define("DEFAULT_DESC","");
 define("DEFAULT_TAGS","");
 
 
-/**
- * Caso true toda vez que submeter um formulario, todos os dados POST
- * serão enviados para o classe Standard e serão validados
- */
-define('USE_STANDARD_VALIDATOR', true);
+/* DEBUG */
+define ("DEBUG" , true);
+
+if(!defined("DEBUG") || DEBUG === false){
+    error_reporting(0);
+    ini_set("display_errors", 0 );
+    ini_set("log_errors", 1);
+    ini_set("error_log", LOG_PATH .DS. "php-error.log");
+    register_shutdown_function( "error_tratamento" );
+    function error_tratamento() {
+        $error = error_get_last();
+        if( $error !== NULL) {
+            $errno   = $error["type"];
+            $errfile = $error["file"];
+            $errline = $error["line"];
+            $errstr  = $error["message"];
 
 
-/**
- * Define o Controller e a view padrão para envio de erro 404
- */
-define('CONTROLLER_404','ErrosController');
-define('ACTION_404','Erro404');
 
-
-//TODO: Tratamento de erro em produção
-
-//register_shutdown_function( "error_tratamento" );
-
-//error_reporting(1);
-
-//function error_tratamento() {
-//    $error = error_get_last();
-//    if( $error !== NULL) {
-//        $errno   = $error["type"];
-//        $errfile = $error["file"];
-//        $errline = $error["line"];
-//        $errstr  = $error["message"];
-//
-//
-//
-//        var_dump($error);
-//    }
-//}
+            var_dump($error);
+        }
+    }
+} else {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1 );
+}
